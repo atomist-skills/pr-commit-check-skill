577bedb028ed4939be71b5469e8b895d02cf42e1
Jim Clark <slimslenderslacks@gmail.com>
2020-08-05 11:29:23 -0700

    Bump pr-commit-check-skill to 0.1.21 and spit metadata

--- src/atomist/skill_meta.cljs
+++ src/atomist/skill_meta.cljs
@@ -3,6 +3,6 @@
 
 (def module-name "pr-commit-check-skill")
 (def path ".")
-(def version "0.1.20")
-(def tag "0.1.20")
-(def generated-at "2020-08-05T18:17:26Z")
+(def version "0.1.21")
+(def tag "0.1.21")
+(def generated-at "2020-08-05T18:29:23Z")
